01dee003b516829d3360ae9028030dcf7b5cc7cc
Yuri Quintero <ingenieraquintero@gmail.com>
2020-05-23 15:11:17 -0500

    Interfaz terminada

--- app/src/main/java/com/example/personas/MainActivity.java
+++ app/src/main/java/com/example/personas/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.personas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import adapterView.OnItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView Lista_Opciones;
+    private String [] opciones;
+    private ArrayAdapter<String> adapter;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Lista_Opciones = findViewById(R.id.listOpciones);
+        opciones = getResources().getStringArray(R.array.opciones);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, opciones);
+        Lista_Opciones.setAdapter(adapter);
+        Lista_Opciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        intent = new Intent(MainActivity.this, CrearPersonas.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(MainActivity.this, ListarPersonas.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
     }
 }
